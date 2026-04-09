751a0224c991f4d71d80a9f8a08dee8b5d61011e
Alex <81166439+AlexCodeMan@users.noreply.github.com>
2026-04-09 16:03:11 +0700

    Initial commit

---
+++ calc/main.py
@@ -0,0 +1,42 @@
+import dearpygui.dearpygui as dpg
+
+
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a * b
+
+
+def div(a, b):
+    return a / b
+
+
+def mod(a, b):
+    return a % b
+
+
+def main():
+    dpg.create_context()
+
+    with dpg.window(tag="Primary Window"):
+        dpg.add_text("Hello, world")
+        dpg.add_button(label="Save")
+        dpg.add_input_text(label="string", default_value="Quick brown fox")
+        dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+
+    dpg.create_viewport(title="Calculator App", width=450, height=600)
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+    dpg.set_primary_window("Primary Window", True)
+    dpg.start_dearpygui()
+    dpg.destroy_context()
+
+
+if __name__ == "__main__":
+    main()
