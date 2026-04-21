09dc97e925ae1ee5598160ac467e68d07509404d
Alex <81166439+AlexCodeMan@users.noreply.github.com>
2026-04-21 19:39:19 +0700

    feat: Added some variables, commands in work

--- todo-cli/main.py
+++ todo-cli/main.py
@@ -1,38 +1,53 @@
-import typer  # noqa: F401
-from rich import print  # noqa: F401
+import typer
+from rich import print
 import json  # noqa: F401
-from pathlib import Path  # noqa: F401
+from pathlib import Path
 
 
 app = typer.Typer()
 todo_list = Path("todo_list.txt")
 
 
-def cr_file(task: str = ""):
-    with open("todo_list.txt", "x"):
-        pass
+MSG_DICT: dict[int, str] = {
+    0: "[ERROR]",
+    1: "[INFO]",
+}
+
+HELP_TEXT: str = """
+Use 'add' to create your task-list and add a first task.
+Use 'ls' to list all your tasks.
+Use 'delete <number>' to delete your tasks or 'delete 0' to delete all.
+"""
 
 
 @app.command()
 def add(task: str):
-    if todo_list.exists():
-        with open("todo_list.txt", "a") as f:
-            f.write(task)
-    else:
-        cr_file(task)
+    with open(todo_list, "a", encoding="utf-8") as f:
+        f.write(task)
 
 
 @app.command()
-def show():
+def ls():
     if todo_list.exists():
-        with open("todo_list.txt", "r") as f:
+        with open(todo_list, "r", encoding="utf-8") as f:
             for i in f:
                 print(i)
+    else:
+        print(
+            MSG_DICT.get(0),
+            "[bold red]File not found![/bold red]",
+            "Use 'add' command to create file and new task.",
+        )
 
 
 @app.command()
-def delete():
+def delete(task_num: int):
     pass
+
+
+@app.command()
+def help():
+    print(HELP_TEXT)
 
 
 if __name__ == "__main__":
