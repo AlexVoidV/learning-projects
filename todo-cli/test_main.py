1c832a6ef5992f8c7addb6716c72ded4ac722480
Alex <81166439+AlexCodeMan@users.noreply.github.com>
2026-04-21 20:41:53 +0700

    test: Added tests for todo-cli

--- todo-cli/test_main.py
+++ todo-cli/test_main.py
@@ -1,1 +1,23 @@
+from click.testing import Result
+from typer.testing import CliRunner
+from main import app
+from pathlib import Path  # noqa: F401
+from rich import print  # noqa: F401
+import json  # noqa: F401
 import pytest  # noqa: F401
+
+
+runner = CliRunner()
+TEST_TASK = "test task"
+
+
+def test_add():
+    result: Result = runner.invoke(app, ["add", TEST_TASK])
+    assert result.exit_code == 0
+    assert "SUCCESS" in result.output
+
+
+def test_ls():
+    result: Result = runner.invoke(app, ["ls"])
+    assert result.exit_code == 0
+    assert TEST_TASK in result.output
