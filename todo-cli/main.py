a8955e6e36fd4554149853d0e75ac7557ab2bb53
Alex <81166439+AlexCodeMan@users.noreply.github.com>
2026-04-21 19:56:33 +0700

    feat: Added magic load & save funcs, remaded add func

--- todo-cli/main.py
+++ todo-cli/main.py
@@ -1,16 +1,17 @@
 import typer
 from rich import print
-import json  # noqa: F401
+import json
 from pathlib import Path
 
 
 app = typer.Typer()
-todo_list = Path("todo_list.txt")
+todo_list = Path("todo_list.json")
 
 
 MSG_DICT: dict[int, str] = {
-    0: "[ERROR]",
-    1: "[INFO]",
+    0: "[[bold green]SUCCESS[/bold green]]",
+    1: "[[bold blue]INFO[/bold blue]]",
+    2: "[[bold red]ERROR[/bold red]]",
 }
 
 HELP_TEXT: str = """
@@ -19,11 +20,27 @@ Use 'ls' to list all your tasks.
 Use 'delete <number>' to delete your tasks or 'delete 0' to delete all.
 """
 
+# TODO: Docstrings
+
+
+def load_tasks() -> list[str]:
+    if todo_list.exists():
+        with open(todo_list, "r", encoding="utf-8") as f:
+            return json.load(f)
+    return []
+
+
+def save_tasks(tasks: list[str]):
+    with open(todo_list, "w", encoding="utf-8") as f:
+        json.dump(tasks, f, ensure_ascii=False, indent=2)
+
 
 @app.command()
-def add(task: str):
-    with open(todo_list, "a", encoding="utf-8") as f:
-        f.write(task)
+def add(task: str = typer.Argument(..., help="Text of new task")):
+    tasks: list[str] = load_tasks()
+    tasks.append(task)
+    save_tasks(tasks)
+    print(MSG_DICT.get(0), f"New task added: {task}.")
 
 
 @app.command()
@@ -34,7 +51,7 @@ def ls():
                 print(i)
     else:
         print(
-            MSG_DICT.get(0),
+            MSG_DICT.get(2),
             "[bold red]File not found![/bold red]",
             "Use 'add' command to create file and new task.",
         )
