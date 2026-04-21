48e50889c5fe418187d1a0a77705096956f22a4e
Alex <81166439+AlexCodeMan@users.noreply.github.com>
2026-04-21 21:40:24 +0700

    test: Added test for delete func

--- todo-cli/test_main.py
+++ todo-cli/test_main.py
@@ -4,18 +4,19 @@ from main import app
 from pathlib import Path  # noqa: F401
 from rich import print  # noqa: F401
 import json  # noqa: F401
-import pytest
+import pytest  # noqa: F401
 
 
 runner = CliRunner()
 TEST_TASK = "test task"
+TEST_TASK_2 = "test task two"
 
 
-@pytest.fixture(autouse=True)
-def use_temp_file(tmp_path, monkeypatch):
-    temp_file = tmp_path / "todo_list.json"
-    monkeypatch.__setattr__("app.todo_list", temp_file)
-    yield temp_file
+# @pytest.fixture(autouse=True)
+# def use_temp_file(tmp_path, monkeypatch):
+#     temp_file = tmp_path / "todo_list.json"
+#     monkeypatch.__setattr__("app.todo_list", temp_file)
+#     yield temp_file
 
 
 def test_add():
@@ -28,3 +29,11 @@ def test_ls():
     result: Result = runner.invoke(app, ["ls"])
     assert result.exit_code == 0
     assert f"1. {TEST_TASK}" in result.output
+
+
+def test_delete():
+    runner.invoke(app, ["add", TEST_TASK_2])
+
+    result: Result = runner.invoke(app, ["delete", "1"])
+    assert result.exit_code == 0
+    assert "SUCCESS" in result.output
