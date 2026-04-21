68de95e740716f23e2fefee902cf318691bc6679
Alex <81166439+AlexCodeMan@users.noreply.github.com>
2026-04-21 21:20:53 +0700

    test: Added temp file for tests

--- todo-cli/test_main.py
+++ todo-cli/test_main.py
@@ -4,11 +4,18 @@ from main import app
 from pathlib import Path  # noqa: F401
 from rich import print  # noqa: F401
 import json  # noqa: F401
-import pytest  # noqa: F401
+import pytest
 
 
 runner = CliRunner()
 TEST_TASK = "test task"
+
+
+@pytest.fixture(autouse=True)
+def use_temp_file(tmp_path, monkeypatch):
+    temp_file = tmp_path / "todo_list.json"
+    monkeypatch.__setattr__("app.todo_list", temp_file)
+    yield temp_file
 
 
 def test_add():
